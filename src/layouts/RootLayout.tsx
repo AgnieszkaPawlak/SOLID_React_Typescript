cfa75fed15db97e0ea342db9116298c83260d0e3
Agnieszka Pawlak <agnieszka.zd@interia.pl>
2023-04-13 14:35:05 +0200

    Revert "Added sass settings"
    This reverts commit 4e6f54bdb9bbbf2b846944a39332d1f29a6583c3.

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
-import { Navbar } from "../components/Navbar";
 
 export const RootLayout = () => {
   return (
     <div>
-      <Navbar/>
       <h1>SOLID Principles</h1>
       <nav>
         <ul>
