6bf7ca7e53c039bc04f253b62d2afc1c8e11e4fe
Agnieszka Pawlak <agnieszka.zd@interia.pl>
2023-04-14 14:36:38 +0200

    Added principle description

--- src/pages/single-responsibility/SingleResponsibility.tsx
+++ src/pages/single-responsibility/SingleResponsibility.tsx
@@ -1,3 +1,13 @@
 export const SingleResponsibility = () => {
-  return <div>Single Responsibility</div>;
+  return(<>
+  <h1>Single Responsibility</h1>
+  <div>
+    <p>
+      This rule helps us to increase the flexability and maintability systems by ensuring that each component is focused on a single task.
+      In React, we can applay this rule by separationg logic of a component into more focused and smaller pices.
+      Example: 
+      The navigation component is responsible for displaying the list with buttons that lead to the pages. Each button has its own separate task, so all functions that are used to handle it should not be in the navigation component.
+    </p>
+  </div>
+  </>) 
 };
